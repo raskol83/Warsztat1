b0c46517324135afb451be66b5e27d51a4a8e79c
Dzmitry Labazinski <d.labazinski@gmail.com>
2024-07-04 00:30:46 +0200

    Changes 2

--- TaskManager/src/main/java/pl/coderslab/Main.java
+++ TaskManager/src/main/java/pl/coderslab/Main.java
@@ -1,7 +1,24 @@
 package pl.coderslab;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
+        {
+            File file = new File("C:\\Users\\Laptop\\Warsztat1\\TaskManager\\src\\main\\java\\pl\\coderslab\\filetoread.txt");
+            Scanner in = null;
+            try {
+                in = new Scanner(file);
+                while (in.hasNext()) {
+                    String line = in.nextLine();
+                    System.out.println(line);
+                }
+            } catch (FileNotFoundException e) {
+
+                e.printStackTrace();
+            }
+
+        }
+    }}
